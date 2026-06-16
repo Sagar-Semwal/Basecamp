7ba0e2c65f78a0d22b5e01eac7d7c6f8dee20aa0
Sagar-Semwal <sagarsemwal7july@gmail.com>
2026-06-16 08:13:27 +0530

    Basic CRUD API

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// const crypto = require('crypto');
+// global.crypto = crypto;
 const express=require('express');
+
 const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/base-camp')
 
@@ -20,10 +24,41 @@ db.once("open",()=>{
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+app.use(express.urlencoded({extended:true}));
+
 
 app.get('/',(req,res)=>{
     res.render('home');
 })
+
+// app.get('/makecampground',async (req,res)=>{
+// const camp= new Campground({title:'My Backyard',description:"Cheap Camping!!"});
+// await camp.save();
+// res.send(camp);
+// });
+app.get('/campgrounds',async (req,res)=>{
+    const campgrounds=await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+})
+
+app.get('/campgrounds/new',(req,res)=>{
+    res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id',async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+})
+
+app.post('/campgrounds',async(req,res)=>{
+    const campground= new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+
+
 app.listen(3000,()=>{
     console.log("Serving on port 3000");
 })
