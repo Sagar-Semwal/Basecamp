adb271a504348aae060a73c1df9169b8275f7ede
Sagar-Semwal <sagarsemwal7july@gmail.com>
2026-06-17 16:14:30 +0530

    EjsMate for boilerplate and layouts

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
+const ejsMate=require('ejs-mate');
 const Campground = require('./models/campground');
+const methodOverride=require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/base-camp')
 
@@ -25,6 +27,8 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.engine('ejs',ejsMate)
 
 
 app.get('/',(req,res)=>{
@@ -56,6 +60,22 @@ app.post('/campgrounds',async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+})
+
+app.put('/campgrounds/:id',async (req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id',async (req,res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+})
 
 
 
